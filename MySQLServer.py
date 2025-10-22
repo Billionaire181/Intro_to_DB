4f3b598b460e91ae9ca445cde7a2798829cd4566
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-10-22 18:54:47 +0100

    initial commit

---
+++ MySQLServer.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from mysql.connector import Error
+
+mydb = mysql.connector.connect(
+    host = "localhost"
+    user = "root"
+    password = "1607"
+)
+cursor = mydb.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXIST alx_book_store")
+print("Database 'alx_book_store' created successfully!")
+
+if mydb.is_connected:
+    print("Connected successfully")
+else:
+    print("Connection Error")
+
+cursor.close()
+mydb.close()
+
